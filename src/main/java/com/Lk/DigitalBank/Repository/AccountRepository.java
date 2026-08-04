c7e571d8f8aadf02443737ac6835f6ef19cdb392
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-03 22:42:04 -0300

    Adicionando método para buscar número da sequence do Banco.

--- src/main/java/com/Lk/DigitalBank/Repository/AccountRepository.java
+++ src/main/java/com/Lk/DigitalBank/Repository/AccountRepository.java
@@ -2,7 +2,11 @@ package com.Lk.DigitalBank.Repository;
 
 import com.Lk.DigitalBank.Entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
+    @Query(value = """
+            SELECT nextval('entitys.account_number_seq')
+            """, nativeQuery = true)
+    Long nextAccountNumber();
 }
