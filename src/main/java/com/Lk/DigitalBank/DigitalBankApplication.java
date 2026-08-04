ecfa6781d610a99908f59b8b51bf5028cec142dd
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-03 21:19:27 -0300

    Primeiro Commit

---
+++ src/main/java/com/Lk/DigitalBank/DigitalBankApplication.java
@@ -0,0 +1,18 @@
+package com.Lk.DigitalBank;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
