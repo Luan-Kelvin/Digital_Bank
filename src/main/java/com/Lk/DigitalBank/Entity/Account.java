83316fa0b2c06de47a19c4b96909ee22b2ec214e
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-03 22:35:59 -0300

    Criando Entidade 'Account'.

---
+++ src/main/java/com/Lk/DigitalBank/Entity/Account.java
@@ -0,0 +1,24 @@
+package com.Lk.DigitalBank.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "accounts", schema = "entitys")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    @ToString.Include
+    @EqualsAndHashCode.Include
+    private String accountNumber;
+
+}
