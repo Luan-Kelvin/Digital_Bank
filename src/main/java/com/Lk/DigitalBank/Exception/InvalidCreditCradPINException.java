4e73bfff60b220f41ae560e5f3b85e73e6912f0e
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-06 00:15:43 -0300

    Criando Exceção par senhas inválidas.

---
+++ src/main/java/com/Lk/DigitalBank/Exception/InvalidCreditCradPINException.java
@@ -0,0 +1,7 @@
+package com.Lk.DigitalBank.Exception;
+
+public class InvalidCreditCradPINException extends RuntimeException {
+    public InvalidCreditCradPINException(String message) {
+        super(message);
+    }
+}
