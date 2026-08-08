1f8d33abe5b7c0e32974414fac121fd3f6c10ff3
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-07 22:55:17 -0300

    Criando método para criar número aleatório de cartão de crédito.

--- src/main/java/com/Lk/DigitalBank/Components/NumberGenerator.java
+++ src/main/java/com/Lk/DigitalBank/Components/NumberGenerator.java
@@ -4,14 +4,31 @@ import com.Lk.DigitalBank.Repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 @RequiredArgsConstructor
-public class AccountNumberGenerator {
+public class NumberGenerator {
+    private final Random random = new Random();
     private final AccountRepository accountRepository;
 
-    public String generate(){
+    public String generateNumberAccount(){
         Long num = accountRepository.nextAccountNumber();
 
         return num + "-1";
+    }
+
+    public String generateNumberCard(){
+        StringBuilder number = new StringBuilder();
+
+        for (int i = 0;i < 16;i++){
+            number.append(random.nextInt(10));
+
+            if ((i + 1 ) % 4 == 0 && i != 15){
+                number.append(" ");
+            }
+        }
+
+        return number.toString();
     }
 }
