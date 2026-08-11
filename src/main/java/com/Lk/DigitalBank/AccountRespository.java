7ab6e270c0d24965f6046da0c317678ae1e2eee2
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-10 22:50:22 -0300

    Criando DerivedQuery para AccountRepository.

--- src/main/java/com/Lk/DigitalBank/AccountRespository.java
+++ src/main/java/com/Lk/DigitalBank/AccountRespository.java
@@ -1,8 +1,41 @@
 package com.Lk.DigitalBank;
 
+import com.Lk.DigitalBank.ENUM.AccountStatus;
+import com.Lk.DigitalBank.ENUM.AccountType;
 import com.Lk.DigitalBank.Entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface AccountRespository extends JpaRepository<Account, Long> {
-    
+
+    // BUSCAR POR NÚMERO DA CONTA
+    Optional<Account> findByAccountNumber(String number);
+
+    // BUSCAR POR TIPO DE CONTA
+    List<Account> findByAccountType(AccountType type);
+
+    // BUSCAR POR STATUS DA CONTA
+    List<Account> findByAccountStatus(AccountStatus status);
+
+    // BUSCANDO POR PERIODO DE CRIAÇÃO
+    List<Account> findByCreationDateBetween(LocalDate min, LocalDate max);
+
+    // BUSCANDO POR ID DE CUSTOMER
+    List<Account> findByCustomerId(Long idCustomer);
+
+    // BUSCANDO POR NOME DO CUSTOMER
+    List<Account> findByCustomerNameIgnoreCase(String name);
+
+    // BUSCANDO POR CPF DE CUSTOMER
+    List<Account> findByCustomerCpf(String cpf);
+
+    //BUSCAR POR ID DE CARTÃO DE CRÉDITO
+    Optional<Account> findByCreditCardId(Long id);
+
+    // BUSCAR POR NÚMERO DO CARTÃO DE CRÉDITO
+    Optional<Account> findByCardNumber(String cardNumber);
+
 }
