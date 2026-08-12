a8266a338218dbe8d57160b0198820213ae01e2b
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-11 23:22:50 -0300

    Adicionando derivedQuery em Customerrepository.

--- src/main/java/com/Lk/DigitalBank/Repository/CustomerRepository.java
+++ src/main/java/com/Lk/DigitalBank/Repository/CustomerRepository.java
@@ -3,6 +3,18 @@ package com.Lk.DigitalBank.Repository;
 import com.Lk.DigitalBank.Entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
+    // BUSCAR POR NOME
+    List<Customer> findByNameContainingignoreCase(String name);
+
+    // BUSCAR POR CPF
+    Optional<Customer> findByCpf(String cpf);
+
+    // BUSCAR POR DATA DE NASCIMENTO
+    List<Customer> findByDateOfBirth(LocalDate date);
 }
