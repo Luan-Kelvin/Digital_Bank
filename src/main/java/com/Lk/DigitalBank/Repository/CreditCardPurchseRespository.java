965254132fc1350da1ab5b841f37553db0463e0b
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-11 23:01:58 -0300

    Criando repositório para CreditCardPurchase.

---
+++ src/main/java/com/Lk/DigitalBank/Repository/CreditCardPurchseRespository.java
@@ -0,0 +1,9 @@
+package com.Lk.DigitalBank.Repository;
+
+import com.Lk.DigitalBank.Entity.CreditCardPurchase;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreditCardPurchseRespository extends JpaRepository<CreditCardPurchase, Long> {
+
+    //
+}
