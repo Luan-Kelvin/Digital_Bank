9e798c92033aaebb4624908dee6a2b0514901327
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-11 22:45:33 -0300

    Criando repositório para CreditCard

---
+++ src/main/java/com/Lk/DigitalBank/Repository/CreditCardRepository.java
@@ -0,0 +1,7 @@
+package com.Lk.DigitalBank.Repository;
+
+import com.Lk.DigitalBank.Entity.CreditCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
+}
