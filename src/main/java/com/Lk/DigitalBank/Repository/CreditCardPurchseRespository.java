2ae3a2b6bf1c18ff1c6abb73eec518d9d9acf603
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-11 23:06:44 -0300

    Adicionando derivedQuery em CreditCardPurchaseRespository

--- src/main/java/com/Lk/DigitalBank/Repository/CreditCardPurchseRespository.java
+++ src/main/java/com/Lk/DigitalBank/Repository/CreditCardPurchseRespository.java
@@ -3,7 +3,23 @@ package com.Lk.DigitalBank.Repository;
 import com.Lk.DigitalBank.Entity.CreditCardPurchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
 public interface CreditCardPurchseRespository extends JpaRepository<CreditCardPurchase, Long> {
 
-    //
+    //BUSCAR POR VALOR TOTAL DA COMPRA
+    List<CreditCardPurchase> findByAmountPurchaseBetween(BigDecimal min, BigDecimal max);
+
+    // BUSCAR POR DATA DE COMPRA
+    List<CreditCardPurchase> findByPurchaseDate(LocalDate min, LocalDate max);
+
+    // BUSCAR POR NOME DO COMÉRCIO
+    List<CreditCardPurchase> findByMerchantIgnoreCase(String merchantName);
+
+    // BUSCAR POR NÚMERO DE CARTÃO
+    List<CreditCardPurchase> findByCreditCardCardNumber(String cardNumber);
+
+
 }
