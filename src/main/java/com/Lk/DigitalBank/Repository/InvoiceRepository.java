871772cdca02e3ccf21b8cccd2eabd9c5cbe2b96
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-11 23:29:31 -0300

    Adicionando derivedQuery em InvoiceRepository

--- src/main/java/com/Lk/DigitalBank/Repository/InvoiceRepository.java
+++ src/main/java/com/Lk/DigitalBank/Repository/InvoiceRepository.java
@@ -3,9 +3,19 @@ package com.Lk.DigitalBank.Repository;
 import com.Lk.DigitalBank.Entity.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
+    // BUSCAR POR DATA DE FECHAMENTO DE FATURA
+    List<Invoice> findByClosingDate(LocalDate date);
 
+    // BUSCAR POR DATA DE VENCIMENTO
+    List<Invoice> findByDueDate(LocalDate date);
+
+    // BUSCAR POR ID DO CARTÃO
+    List<Invoice> findByCreditCardId(Long id);
+
+    //
 }
