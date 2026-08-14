a6f39691bace75fe46ebd072b00680db45cf5ff4
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-13 22:52:37 -0300

    Criando método em Service para criar Cliente.

--- src/main/java/com/Lk/DigitalBank/Services/CustomerService/CustomerPostService.java
+++ src/main/java/com/Lk/DigitalBank/Services/CustomerService/CustomerPostService.java
@@ -1,17 +1,38 @@
 package com.Lk.DigitalBank.Services.CustomerService;
 
+import com.Lk.DigitalBank.Conversores.Conversor;
+import com.Lk.DigitalBank.DTOs.Customer.CustomerGetDTO;
+import com.Lk.DigitalBank.DTOs.Customer.CustomerPostDTO;
+import com.Lk.DigitalBank.Entity.Customer;
+import com.Lk.DigitalBank.Exception.CustomerAlreadyExistsException;
 import com.Lk.DigitalBank.Repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CustomerPostService {
     private final Logger logger = LoggerFactory.getLogger(CustomerPostService.class);
+    private final Conversor conversor;
     private final CustomerRepository customerRepository;
 
     // CRIAR CUSTOMER
-    public
+    public CustomerGetDTO createCustomer(CustomerPostDTO customerPostDTO){
+        Optional<Customer> customerRep = customerRepository.findByCpf(customerPostDTO.cpf());
+
+        if (customerRep.isPresent()){
+            throw new CustomerAlreadyExistsException(String.format("ERRO! Cliente com CPF %s já existe.", customerPostDTO.cpf()));
+        }
+
+        Customer customer = new Customer(customerPostDTO.name(), customerPostDTO.cpf(), customerPostDTO.dateOfBirth());
+        customerRepository.save(customer);
+
+        logger.info(String.format("Cliente %s cadastrado com sucesso!", customer.getName()));
+
+        return conversor.converterCustomer(customer);
+    }
 }
