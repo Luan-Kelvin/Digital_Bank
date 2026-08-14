ddf37b02e68bbed56c4cb81c51d6120a17711b36
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-13 22:51:08 -0300

    Atualizando atributo de DTO para List.

--- src/main/java/com/Lk/DigitalBank/DTOs/Customer/CustomerGetDTO.java
+++ src/main/java/com/Lk/DigitalBank/DTOs/Customer/CustomerGetDTO.java
@@ -1,11 +1,14 @@
 package com.Lk.DigitalBank.DTOs.Customer;
 
+import com.Lk.DigitalBank.Entity.Account;
+
 import java.time.LocalDate;
+import java.util.List;
 
 public record CustomerGetDTO(
         Long id,
         String name,
         LocalDate dateOfBirth,
-        Long AccountID
+        List<Account> Accounts
 ) {
 }
