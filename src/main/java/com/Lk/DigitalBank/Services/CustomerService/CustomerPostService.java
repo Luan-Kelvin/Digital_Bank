4401c68c520da4712c9144a5c610764606a0d1bd
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-13 22:28:05 -0300

    Criando Classe de Serviço para Customer com requisições POST.

---
+++ src/main/java/com/Lk/DigitalBank/Services/CustomerService/CustomerPostService.java
@@ -0,0 +1,17 @@
+package com.Lk.DigitalBank.Services.CustomerService;
+
+import com.Lk.DigitalBank.Repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerPostService {
+    private final Logger logger = LoggerFactory.getLogger(CustomerPostService.class);
+    private final CustomerRepository customerRepository;
+
+    // CRIAR CUSTOMER
+    public
+}
