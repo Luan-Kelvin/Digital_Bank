5b8a8fb5fc23c48fac0a4f0d6d4da83201f57bc6
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-15 17:42:00 -0300

    Ajustanto Serviçoes gerais de Account.

--- src/main/java/com/Lk/DigitalBank/Services/AccountService/AccountServiceGeneral.java
+++ src/main/java/com/Lk/DigitalBank/Services/AccountService/AccountServiceGeneral.java
@@ -1,13 +1,11 @@
 package com.Lk.DigitalBank.Services.AccountService;
 
-import com.Lk.DigitalBank.Components.NumberGenerator;
-import com.Lk.DigitalBank.Conversores.Conversor;
 import com.Lk.DigitalBank.ENUM.TransactionType;
 import com.Lk.DigitalBank.Entity.Account;
 import com.Lk.DigitalBank.Entity.Transaction;
 import com.Lk.DigitalBank.Exception.AccountDoesNotExistException;
+import com.Lk.DigitalBank.Exception.AccountInactiveException;
 import com.Lk.DigitalBank.Repository.AccountRepository;
-import com.Lk.DigitalBank.Repository.CustomerRepository;
 import com.Lk.DigitalBank.Repository.TransactionRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -20,12 +18,9 @@ import java.math.BigDecimal;
 @Service
 @RequiredArgsConstructor
 public class AccountServiceGeneral {
-    private final Logger logger = LoggerFactory.getLogger(AccountGetService.class);
-    private final Conversor conversor;
+    private final Logger logger = LoggerFactory.getLogger(AccountServiceGeneral.class);
     private final AccountRepository accountRepository;
     private final TransactionRepository transactionRepository;
-    private final CustomerRepository customerRepository;
-    private final NumberGenerator numberGenerator;
 
     // DEPOSITAR DINHEIRO
     @Transactional
@@ -33,10 +28,14 @@ public class AccountServiceGeneral {
         Account account = accountRepository.findByAccountNumber(numberAccount)
                 .orElseThrow(() -> new AccountDoesNotExistException(String.format("ERRO! Conta com número %s não existe.", numberAccount)));
 
+        if (!account.isActive()){
+            throw new AccountInactiveException("ERRO! Status de conta esta inátivo.");
+        }
+
         account.deposit(value);
 
         String descriprion = String.format("Déposito feito no valor de R$%s", value);
-        Transaction transaction = new Transaction(TransactionType.DEPOSIT, value, descriprion, account);
+        Transaction transaction = new Transaction(TransactionType.DEPOSIT, value, descriprion);
 
         account.addTransaction(transaction);
         transactionRepository.save(transaction);
@@ -50,10 +49,14 @@ public class AccountServiceGeneral {
         Account account = accountRepository.findByAccountNumber(numberAccount)
                 .orElseThrow(() -> new AccountDoesNotExistException(String.format("ERRO! Conta com número %s não existe.", numberAccount)));
 
+        if (!account.isActive()){
+            throw new AccountInactiveException("ERRO! Status de conta esta inátivo.");
+        }
+
         account.withdraw(value);
 
         String description = String.format("Saque feito no valor de R$%s", value);
-        Transaction transaction = new Transaction(TransactionType.WITHDRAW, value, description, account);
+        Transaction transaction = new Transaction(TransactionType.WITHDRAW, value, description);
 
         account.addTransaction(transaction);
         transactionRepository.save(transaction);
