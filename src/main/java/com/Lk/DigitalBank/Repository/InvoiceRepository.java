ca6de2db0b317c913455ce5ad18ab9e105254190
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-16 17:21:20 -0300

    Adicionando método par buscra ftur por mes

--- src/main/java/com/Lk/DigitalBank/Repository/InvoiceRepository.java
+++ src/main/java/com/Lk/DigitalBank/Repository/InvoiceRepository.java
@@ -1,10 +1,13 @@
 package com.Lk.DigitalBank.Repository;
 
+import com.Lk.DigitalBank.Entity.CreditCard;
 import com.Lk.DigitalBank.Entity.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.List;
+import java.util.Optional;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
@@ -17,5 +20,6 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
     // BUSCAR POR ID DO CARTÃO
     List<Invoice> findByCreditCardId(Long id);
 
-    //
+    // BUSCAR POR CARTÃO DE CRÉDITO E MES DE REFERÊNCIA
+    Optional<Invoice> findByCreditCardAndReferenceMonth(CreditCard creditCard, YearMonth yearMonth);
 }
