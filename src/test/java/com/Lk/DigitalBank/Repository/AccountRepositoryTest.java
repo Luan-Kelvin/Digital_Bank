05bc33d57130dd53f87eab4e43602732a8d48034
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-20 21:24:41 -0300

    Criando teste para verificar se repositorio busca todas as contas do banco.

--- src/test/java/com/Lk/DigitalBank/Repository/AccountRepositoryTest.java
+++ src/test/java/com/Lk/DigitalBank/Repository/AccountRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,5 +30,23 @@ public class AccountRepositoryTest {
         assertTrue(ac.isPresent());
 
         assertEquals("12345", ac.get().getAccountNumber());
+    }
+
+    @Test
+    public void deveBuscarTodasAsContasDoBanco(){
+        Account account1 = new Account();
+        Account account2 = new Account();
+
+        account1.addNumberAccount("12345");
+        account2.addNumberAccount("54321");
+
+        accountRepository.save(account1);
+        accountRepository.save(account2);
+
+        List<Account> accounts = accountRepository.findAll();
+
+        assertEquals(2, accounts.size());
+        assertEquals("12345", accounts.get(0).getAccountNumber());
+        assertEquals("54321", accounts.get(1).getAccountNumber());
     }
 }
