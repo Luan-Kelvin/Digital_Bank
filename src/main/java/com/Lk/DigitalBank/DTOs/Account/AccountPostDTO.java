2171b38e2a354ccd3e1c3fed4d9477709eaee070
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-21 23:46:50 -0300

    Criando DTO para receber requisições POST para Account

--- src/main/java/com/Lk/DigitalBank/DTOs/Account/AccountPostDTO.java
+++ src/main/java/com/Lk/DigitalBank/DTOs/Account/AccountPostDTO.java
@@ -1,9 +1,17 @@
 package com.Lk.DigitalBank.DTOs.Account;
 
 import com.Lk.DigitalBank.ENUM.AccountType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record AccountPostDTO(
+
+        @NotBlank
+        @Pattern(regexp = "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$")
         String cpfCustomer,
+
+        @NotNull
         AccountType accountType
 ) {
 }
