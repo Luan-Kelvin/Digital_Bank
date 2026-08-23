f90cc20691cfa1c63904fcee384b866b2bda9126
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-23 20:11:08 -0300

    Criando uma classe de Test para testar metodos de AccountDeleteService

---
+++ src/test/java/com/Lk/DigitalBank/Services/AccountService/AccountDeleteServiceTest.java
@@ -0,0 +1,53 @@
+package com.Lk.DigitalBank.Services.AccountService;
+
+import com.Lk.DigitalBank.ENUM.AccountStatus;
+import com.Lk.DigitalBank.Entity.Account;
+import com.Lk.DigitalBank.Exception.AccountDoesNotExistException;
+import com.Lk.DigitalBank.Repository.AccountRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+public class AccountDeleteServiceTest {
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
+    private AccountDeleteService accountDeleteService;
+
+    @Test
+    public void deveMudarOStatusDaContaParaBlocked(){
+        Account account = new Account();
+        account.addNumberAccount("12345");
+
+        when(accountRepository.findByAccountNumber(account.getAccountNumber())).thenReturn(Optional.of(account));
+
+        accountDeleteService.deleteAccount(account.getAccountNumber());
+
+        assertEquals(AccountStatus.BLOCKED, account.getAccountStatus());
+
+        verify(accountRepository).findByAccountNumber(account.getAccountNumber());
+    }
+
+    @Test
+    public void deveLancarExcecaoSeAccountnaoExistir(){
+
+        when(accountRepository.findByAccountNumber("12345")).thenReturn(Optional.empty());
+
+        assertThrows(AccountDoesNotExistException.class, () -> accountDeleteService.deleteAccount("12345"));
+
+        verify(accountRepository).findByAccountNumber("12345");
+    }
+}
