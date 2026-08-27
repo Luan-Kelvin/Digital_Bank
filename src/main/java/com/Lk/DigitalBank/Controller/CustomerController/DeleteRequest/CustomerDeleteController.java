49d53d98db89a1e3d5f435ff2d44f453a67b8b6a
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-27 19:54:22 -0300

    Criando Controller para receber requisições DELETE pr customer.

---
+++ src/main/java/com/Lk/DigitalBank/Controller/CustomerController/DeleteRequest/CustomerDeleteController.java
@@ -0,0 +1,24 @@
+package com.Lk.DigitalBank.Controller.CustomerController.DeleteRequest;
+
+import com.Lk.DigitalBank.Services.CustomerService.CustomerDeleteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customers")
+@RequiredArgsConstructor
+public class CustomerDeleteController {
+    private final CustomerDeleteService customerDeleteService;
+
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable("id") Long id){
+        customerDeleteService.deleteCustomner(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
