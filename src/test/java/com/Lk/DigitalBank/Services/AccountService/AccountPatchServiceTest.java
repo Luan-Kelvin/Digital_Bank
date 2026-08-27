f35e714f51eaf7ee3ff0608c211a7f531cf002d2
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-26 22:10:54 -0300

    Alterando teste de mudança de type..

--- src/test/java/com/Lk/DigitalBank/Services/AccountService/AccountPatchServiceTest.java
+++ src/test/java/com/Lk/DigitalBank/Services/AccountService/AccountPatchServiceTest.java
@@ -1,6 +1,10 @@
 package com.Lk.DigitalBank.Services.AccountService;
 
 
+import com.Lk.DigitalBank.Conversores.Conversor;
+import com.Lk.DigitalBank.DTOs.Account.AccountGetDTO;
+import com.Lk.DigitalBank.DTOs.Account.AccountPatchDTO;
+import com.Lk.DigitalBank.ENUM.AccountStatus;
 import com.Lk.DigitalBank.ENUM.AccountType;
 import com.Lk.DigitalBank.Entity.Account;
 import com.Lk.DigitalBank.Entity.Customer;
@@ -11,10 +15,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -24,6 +28,9 @@ public class AccountPatchServiceTest {
     @Mock
     private AccountRepository accountRepository;
 
+    @Mock
+    private Conversor conversor;
+
     @InjectMocks
     private AccounPatchService accounPatchService;
 
@@ -31,13 +38,16 @@ public class AccountPatchServiceTest {
     public void deveAlterarTypeDaConta(){
         Customer customer = new Customer();
         Account account = new Account(customer, AccountType.CURRENT);
+        AccountGetDTO dto = new AccountGetDTO("12345", BigDecimal.valueOf(200.0), AccountType.SAVINGS, AccountStatus.ACTIVE, 1L, "Joaquim");
+        AccountPatchDTO pathDto = new AccountPatchDTO("12345", AccountType.SAVINGS);
         account.addNumberAccount("12345");
 
         when(accountRepository.findByAccountNumber("12345")).thenReturn(Optional.of(account));
+        when(conversor.converterAccount(account)).thenReturn(dto);
 
-        accounPatchService.updateType("savings", account.getAccountNumber());
+        AccountGetDTO accountGetDTO = accounPatchService.updateType(pathDto);
 
-        assertEquals(AccountType.SAVINGS,  account.getAccountType());
+        assertEquals(AccountType.SAVINGS,  accountGetDTO.accountType());
 
         verify(accountRepository).findByAccountNumber(account.getAccountNumber());
     }
