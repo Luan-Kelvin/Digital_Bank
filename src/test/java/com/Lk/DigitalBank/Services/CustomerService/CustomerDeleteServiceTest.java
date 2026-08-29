6ba79ca038754b4606d3a87c2d85abbcb969f84d
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-29 18:40:53 -0300

    Alterando Teste que verifica se cliente ficou inativo

--- src/test/java/com/Lk/DigitalBank/Services/CustomerService/CustomerDeleteServiceTest.java
+++ src/test/java/com/Lk/DigitalBank/Services/CustomerService/CustomerDeleteServiceTest.java
@@ -1,8 +1,14 @@
 package com.Lk.DigitalBank.Services.CustomerService;
 
+import com.Lk.DigitalBank.ENUM.AccountStatus;
+import com.Lk.DigitalBank.ENUM.AccountType;
+import com.Lk.DigitalBank.ENUM.CustomerStatus;
+import com.Lk.DigitalBank.Entity.Account;
 import com.Lk.DigitalBank.Entity.Customer;
 import com.Lk.DigitalBank.Exception.CustomerDoesNotExistException;
+import com.Lk.DigitalBank.Repository.AccountRepository;
 import com.Lk.DigitalBank.Repository.CustomerRepository;
+import com.Lk.DigitalBank.Services.AccountService.AccountPatchService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,6 +18,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -21,19 +28,32 @@ public class CustomerDeleteServiceTest {
     @Mock
     private CustomerRepository customerRepository;
 
+    @Mock
+    private AccountPatchService accountPatchService;
+
+    @Mock
+    private AccountRepository accountRepository;
+
     @InjectMocks
     private CustomerDeleteService customerDeleteService;
 
     @Test
-    public void deveDeletarCustomer(){
+    public void deveDeixarCustomerComStatusInativo(){
         Long id = 1L;
         Customer customer = new Customer("Jonas", "123.456.789-10", LocalDate.of(2002, 10, 1));
+        Account account = new Account(customer, AccountType.CURRENT);
+        customer.addAccount(account);
+        account.addCustomer(customer);
 
         when(customerRepository.findById(id)).thenReturn(Optional.of(customer));
 
         customerDeleteService.deleteCustomner(id);
 
-        verify(customerRepository).delete(customer);
+        assertEquals(CustomerStatus.INACTIVE, customer.getCustomerStatus());
+
+        verify(accountPatchService).blockAccount(account.getId());
+        verify(accountRepository).save(any(Account.class));
+        verify(customerRepository).save(any(Customer.class));
 
     }
 
