99679ab3c1a88bffc7da9c47084b54623e2237d9
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-29 16:43:18 -0300

    Alterando metodo de deletação de Customer.

--- src/main/java/com/Lk/DigitalBank/Services/CustomerService/CustomerDeleteService.java
+++ src/main/java/com/Lk/DigitalBank/Services/CustomerService/CustomerDeleteService.java
@@ -2,7 +2,9 @@ package com.Lk.DigitalBank.Services.CustomerService;
 
 import com.Lk.DigitalBank.Entity.Customer;
 import com.Lk.DigitalBank.Exception.CustomerDoesNotExistException;
+import com.Lk.DigitalBank.Repository.AccountRepository;
 import com.Lk.DigitalBank.Repository.CustomerRepository;
+import com.Lk.DigitalBank.Services.AccountService.AccountPatchService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,13 +14,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomerDeleteService {
     private final Logger logger = LoggerFactory.getLogger(CustomerDeleteService.class);
+    private final AccountPatchService accountPatchService;
+    private final AccountRepository accountRepository;
     private final CustomerRepository customerRepository;
 
     public void deleteCustomner(Long id){
         Customer customer = customerRepository.findById(id)
                 .orElseThrow(() -> new CustomerDoesNotExistException(String.format("ERRO! Cliente com ID = %s não existe.", id)));
 
-        customerRepository.delete(customer);
+        customer.getAccounts().forEach(ac -> {
+            accountPatchService.blockAccount(ac.getId());
+            accountRepository.save(ac);
+        });
+
+        customer.inactive();
+        customerRepository.save(customer);
         logger.info(String.format("Cliente %s deletado com sucesso!", customer.getName()));
     }
 }
