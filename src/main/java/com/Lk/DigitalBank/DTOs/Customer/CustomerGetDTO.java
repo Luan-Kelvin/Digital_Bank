bda2fe1b04719586bda6dd8cde32abf40351bb89
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-29 16:35:39 -0300

    Adicionando atributo CustomerStatus para DTOGET de Customer.

--- src/main/java/com/Lk/DigitalBank/DTOs/Customer/CustomerGetDTO.java
+++ src/main/java/com/Lk/DigitalBank/DTOs/Customer/CustomerGetDTO.java
@@ -1,5 +1,7 @@
 package com.Lk.DigitalBank.DTOs.Customer;
 
+import com.Lk.DigitalBank.ENUM.CustomerStatus;
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -7,6 +9,7 @@ public record CustomerGetDTO(
         Long id,
         String name,
         LocalDate dateOfBirth,
+        CustomerStatus status,
         List<String> Accounts
 ) {
 }
