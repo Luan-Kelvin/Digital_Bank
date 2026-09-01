1f5afa30b7464cfd136402b4d6c477fe8ac248f3
Luan Kelvin <kelvinluan897@gmail.com>
2026-08-31 23:12:19 -0300

    Criando métodos para listar crtões de credito de acordo com seu status.

--- src/main/java/com/Lk/DigitalBank/Services/CreditCardService/CreditCardGetService.java
+++ src/main/java/com/Lk/DigitalBank/Services/CreditCardService/CreditCardGetService.java
@@ -3,6 +3,7 @@ package com.Lk.DigitalBank.Services.CreditCardService;
 import com.Lk.DigitalBank.Conversores.Conversor;
 import com.Lk.DigitalBank.DTOs.CreditCard.CreditCardGetDTO;
 import com.Lk.DigitalBank.Entity.CreditCard;
+import com.Lk.DigitalBank.Exception.CreditCardsNotExistException;
 import com.Lk.DigitalBank.Repository.CreditCardRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -19,18 +20,69 @@ public class CreditCardGetService {
     private final Conversor conversor;
     private final CreditCardRepository creditCardRepository;
 
-    // LISTAR CREDIT CARD
+    // LISTAR CREDIT CARD ATIVOS
     public List<CreditCardGetDTO> listCreditCards(){
-        List<CreditCard> cards = creditCardRepository.findAll();
+        List<CreditCard> cards = creditCardRepository.searchCreditCardActive();
 
         if (cards.isEmpty()){
-            logger.info("Não foi localizado nenhum cartão de crédito cadastrado no banco.");
+            logger.info("Não foi localizado nenhum cartão de crédito  ativo no banco.");
             return List.of();
         }
 
+        return converterLista(cards);
+    }
+
+    // LISTAR CREDIT CARD BLOQUEADOS
+    public List<CreditCardGetDTO> listCreditCardsBlockeds(){
+        List<CreditCard> cards = creditCardRepository.searchCreditCardBlocked();
+
+        if (cards.isEmpty()){
+            logger.info("Não foi localizado nenhum cartão de crédito Bloqueado no banco.");
+            return List.of();
+        }
+
+        return converterLista(cards);
+    }
+
+    // LISTAR CREDIT CARD EXPIRED
+    public List<CreditCardGetDTO> listCreditCardExpired(){
+        List<CreditCard> cards = creditCardRepository.searchCreditCardExpired();
+
+        if (cards.isEmpty()){
+            logger.info("Não foi localizado nenhum cartão de crédito expirado no banco.");
+            return List.of();
+        }
+
+        return converterLista(cards);
+
+    }
+
+    // LISTAR CREDIT CARD CANCELED
+    public List<CreditCardGetDTO> listCreditCardCanceled(){
+        List<CreditCard> cards = creditCardRepository.searchCreditCardCanceled();
+
+        if (cards.isEmpty()){
+            logger.info("Não foi localizado nenhum cartão de crédito cancelado no banco.");
+            return List.of();
+        }
+
+        return converterLista(cards);
+    }
+
+    // BUSCAR CREDIT CARD POR NÚMERO
+    public CreditCardGetDTO findByCardNumber(String number){
+        CreditCard card = creditCardRepository.findByCardNumber(number)
+                .orElseThrow(() -> new CreditCardsNotExistException(String.format("ERRO! Cart~o com número %s não existe no banco.", number)));
+
+        return conversor.converterCreditCard(card);
+    }
+
+
+    // Cnverter Listas de CreditCard em dtos
+    private List<CreditCardGetDTO> converterLista(List<CreditCard> list){
         List<CreditCardGetDTO> dto = new ArrayList<>();
 
-        cards.forEach(c -> {
+        list.forEach(c -> {
             dto.add(conversor.converterCreditCard(c));
         });
 
