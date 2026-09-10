aba02cc11815b6d8f2b80f8ac2161ab388ea2453
Luan Kelvin <kelvinluan897@gmail.com>
2026-09-09 23:10:29 -0300

    Criando teste unitarios para metodo transferPix

--- src/test/java/com/Lk/DigitalBank/Services/AccountService/AccountPostServiceTest.java
+++ src/test/java/com/Lk/DigitalBank/Services/AccountService/AccountPostServiceTest.java
@@ -4,15 +4,18 @@ import com.Lk.DigitalBank.Components.NumberGenerator;
 import com.Lk.DigitalBank.Conversores.Conversor;
 import com.Lk.DigitalBank.DTOs.Account.AccountGetDTO;
 import com.Lk.DigitalBank.DTOs.Account.AccountPostDTO;
+import com.Lk.DigitalBank.DTOs.Account.TransferPixDTO;
 import com.Lk.DigitalBank.ENUM.AccountStatus;
 import com.Lk.DigitalBank.ENUM.AccountType;
+import com.Lk.DigitalBank.ENUM.TransactionType;
 import com.Lk.DigitalBank.Entity.Account;
 import com.Lk.DigitalBank.Entity.Customer;
-import com.Lk.DigitalBank.Exception.AccountAlreadyExistsException;
-import com.Lk.DigitalBank.Exception.CustomerDoesNotExistException;
+import com.Lk.DigitalBank.Entity.Transaction;
+import com.Lk.DigitalBank.Exception.*;
 import com.Lk.DigitalBank.Repository.AccountRepository;
 import com.Lk.DigitalBank.Repository.CustomerRepository;
 
+import com.Lk.DigitalBank.Repository.TransactionRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,6 +27,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -45,6 +49,9 @@ public class AccountPostServiceTest {
 
     @Mock
     private  CustomerRepository customerRepository;
+
+    @Mock
+    private TransactionRepository transactionRepository;
 
     @InjectMocks
     private  AccountPostService accountPostService;
@@ -123,4 +130,187 @@ public class AccountPostServiceTest {
         verify(numberGenerator, never()).generateNumberAccount();
         verify(accountRepository, never()).save(any(Account.class));
     }
+
+    @Test
+    @DisplayName("Deve realizar uma transferêcia pix entre duas contas e salvar essas transferência e retornar um dto")
+    void deveRealizarTransferenciaPixComSuceso(){
+        Customer customer1 = new Customer("Ronaldinho", "123.456.789-10", LocalDate.of(2005, 12, 15));
+        Customer customer2 = new Customer("Romario", "222.333.444-19", LocalDate.of(2005, 12, 15));
+
+
+        Account recipient = new Account(customer1, AccountType.CURRENT);
+        recipient.addNumberAccount("2020 2020 2020 2020");
+
+        Account sender = new Account(customer2, AccountType.CURRENT);
+        sender.addNumberAccount("1010 1010 1010 1010");
+
+        sender.deposit(BigDecimal.valueOf(500));
+
+        TransferPixDTO dtoPix = new TransferPixDTO(
+                "1010 1010 1010 1010",
+                "2020 2020 2020 2020",
+                BigDecimal.valueOf(200)
+        );
+
+        when(accountRepository.findByAccountNumber("2020 2020 2020 2020")).thenReturn(Optional.of(recipient));
+        when(accountRepository.findByAccountNumber("1010 1010 1010 1010")).thenReturn(Optional.of(sender));
+
+        accountPostService.transferViaPix(dtoPix);
+
+        ArgumentCaptor<Account> accountCaptor = ArgumentCaptor.forClass(Account.class);
+
+        verify(accountRepository, times(2)).save(accountCaptor.capture());
+
+        List<Account> accountList = accountCaptor.getAllValues();
+
+        Account accountSender = accountList.get(0);
+        Account accountRecipient = accountList.get(1);
+
+        ArgumentCaptor<Transaction> transactionCaptor = ArgumentCaptor.forClass(Transaction.class);
+
+        verify(transactionRepository, times(2)).save(transactionCaptor.capture());
+
+        List<Transaction> transactionList = transactionCaptor.getAllValues();
+
+        Transaction transactionSender = transactionList.get(0);
+        Transaction transactionRecipient = transactionList.get(1);
+
+        assertEquals(transactionSender.getTransactionValue(), dtoPix.value());
+        assertEquals(transactionSender.getAccount().getAccountNumber(), dtoPix.senderAccount());
+        assertEquals( TransactionType.PIX_SENT, transactionSender.getTransactionType());
+
+        assertEquals(transactionRecipient.getTransactionValue(), dtoPix.value());
+        assertEquals(transactionRecipient.getAccount().getAccountNumber(), dtoPix.recipientAccount());
+        assertEquals( TransactionType.PIX_RECEVIED, transactionRecipient.getTransactionType());
+
+        assertEquals(BigDecimal.valueOf(300), accountSender.getBalance());
+        assertEquals(dtoPix.senderAccount(), accountSender.getAccountNumber());
+
+        assertEquals(BigDecimal.valueOf(200), accountRecipient.getBalance());
+        assertEquals(dtoPix.recipientAccount(), accountRecipient.getAccountNumber());
+
+        verify(accountRepository).findByAccountNumber("2020 2020 2020 2020");
+        verify(accountRepository).findByAccountNumber("1010 1010 1010 1010");
+    }
+
+
+    @Test
+    @DisplayName("Deve lançar exceção se conta não existir")
+    void deveLancarExcecaoSeContaNaoExistir(){
+        TransferPixDTO dtoPix = new TransferPixDTO(
+                "1010 1010 1010 1010",
+                "2020 2020 2020 2020",
+                BigDecimal.valueOf(200)
+        );
+
+        when(accountRepository.findByAccountNumber("2020 2020 2020 2020")).thenReturn(Optional.empty());
+
+        assertThrows(AccountDoesNotExistException.class, () -> accountPostService.transferViaPix(dtoPix));
+
+        verify(accountRepository, never()).save(any(Account.class));
+        verifyNoInteractions(transactionRepository);
+    }
+
+    @Test
+    @DisplayName("Lançar exceção se conta do cliente que recebe o pix seja inativa")
+    void deveLancarExcecaoSeContaDoRecipientEstejaInativa(){
+        Account recipient = new Account();
+        recipient.addNumberAccount("2020 2020 2020 2020");
+        recipient.blockedAccount();
+
+        Account sender = new Account();
+        sender.addNumberAccount("1010 1010 1010 1010");
+
+        TransferPixDTO dtoPix = new TransferPixDTO(
+                "1010 1010 1010 1010",
+                "2020 2020 2020 2020",
+                BigDecimal.valueOf(200)
+        );
+
+        when(accountRepository.findByAccountNumber(recipient.getAccountNumber())).thenReturn(Optional.of(recipient));
+        when(accountRepository.findByAccountNumber(sender.getAccountNumber())).thenReturn(Optional.of(sender));
+
+
+        assertThrows(AccountInactiveException.class, () -> accountPostService.transferViaPix(dtoPix));
+
+        verify(accountRepository, never()).save(any(Account.class));
+        verifyNoInteractions(transactionRepository);
+    }
+
+    @Test
+    @DisplayName("Lançar exceção se conta do cliente que envia o pix seja inativa")
+    void deveLancarExcecaoSeContaDoSenderEstejaInativa(){
+        Account recipient = new Account();
+        recipient.addNumberAccount("2020 2020 2020 2020");
+
+        Account sender = new Account();
+        sender.addNumberAccount("1010 1010 1010 1010");
+        sender.blockedAccount();
+
+        TransferPixDTO dtoPix = new TransferPixDTO(
+                "1010 1010 1010 1010",
+                "2020 2020 2020 2020",
+                BigDecimal.valueOf(200)
+        );
+
+        when(accountRepository.findByAccountNumber(recipient.getAccountNumber())).thenReturn(Optional.of(recipient));
+        when(accountRepository.findByAccountNumber(sender.getAccountNumber())).thenReturn(Optional.of(sender));
+
+
+        assertThrows(AccountInactiveException.class, () -> accountPostService.transferViaPix(dtoPix));
+
+        verify(accountRepository, never()).save(any(Account.class));
+        verifyNoInteractions(transactionRepository);
+    }
+
+    @Test
+    @DisplayName("Lançar exceção se valor da transferência for igual a 0")
+    void deveLancarExcecaoSeValorFor0(){
+        Account recipient = new Account();
+        recipient.addNumberAccount("2020 2020 2020 2020");
+
+        Account sender = new Account();
+        sender.addNumberAccount("1010 1010 1010 1010");
+
+        TransferPixDTO dtoPix = new TransferPixDTO(
+                "1010 1010 1010 1010",
+                "2020 2020 2020 2020",
+                BigDecimal.ZERO
+        );
+
+        when(accountRepository.findByAccountNumber(recipient.getAccountNumber())).thenReturn(Optional.of(recipient));
+        when(accountRepository.findByAccountNumber(sender.getAccountNumber())).thenReturn(Optional.of(sender));
+
+
+        assertThrows(IllegalArgumentException.class, () -> accountPostService.transferViaPix(dtoPix));
+
+        verify(accountRepository, never()).save(any(Account.class));
+        verifyNoInteractions(transactionRepository);
+    }
+
+    @Test
+    @DisplayName("Lançar exceção se contas forem iguais")
+    void deveLancarExcecaoSeContasForemIguais(){
+        Account recipient = new Account();
+        recipient.addNumberAccount("2020 2020 2020 2020");
+
+        Account sender = new Account();
+        sender.addNumberAccount("2020 2020 2020 2020");
+
+        TransferPixDTO dtoPix = new TransferPixDTO(
+                "2020 2020 2020 2020",
+                "2020 2020 2020 2020",
+                BigDecimal.valueOf(200)
+        );
+
+        when(accountRepository.findByAccountNumber(recipient.getAccountNumber())).thenReturn(Optional.of(recipient));
+        when(accountRepository.findByAccountNumber(sender.getAccountNumber())).thenReturn(Optional.of(sender));
+
+
+        assertThrows(PixTransferFailedException.class, () -> accountPostService.transferViaPix(dtoPix));
+
+        verify(accountRepository, never()).save(any(Account.class));
+        verifyNoInteractions(transactionRepository);
+    }
+
 }
